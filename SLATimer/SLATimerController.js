b13400a4f5b32b25616e293014e07f1b2c5e98b3
Ramesh Sanjaya <53003946+Ramesh9969@users.noreply.github.com>
2022-12-03 17:27:47 +0530

    Add files via upload

---
+++ SLATimer/SLATimerController.js
@@ -0,0 +1,100 @@
+({  
+    doInit : function(component, event, helper) {
+        
+        console.log("diinit get called!!");
+        var id = component.get("v.recordId");
+        
+        var tts = 'ddr';
+        var ttp = 'gg';
+        var action = component.get("c.SLATimeMethod");
+        action.setParams({ "recordId": id });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+               var ttp = response.getReturnValue();
+                 if(ttp != null){
+                  var nn = ttp.split(":");
+                     if(nn[4]=='minus'){
+                        component.set("v.plus","false"); 
+                     }
+                     if(nn[4]=='plus'){
+                         component.set("v.plus","true");   
+                     }
+                 }
+                
+                    tts = nn[0]+":"+nn[1]+":"+nn[2];
+            
+                component.set("v.ltngTimmer",tts);
+                     
+              
+                     var ss = tts.split(":");
+                     var days = ((ss[0]-(ss[0]%24))/24);
+                    component.set("v.days",days);  
+                  
+              
+                if( nn[3] == 'false'){
+                      helper.setStartTimeOnUI(component); 
+                }else{
+                   helper.setStopTimeOnUI(component);
+                 }
+                
+                
+                
+                
+                
+                
+            }});
+        $A.enqueueAction(action);
+        
+        
+    },
+    changeField: function(component, event, helper) { 
+        
+        console.log("changefiel get called!!");
+        var id = component.get("v.recordId");
+        
+    
+        var action = component.get("c.SLATimeMethod");
+        action.setParams({ "recordId": id });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+              var ttp = response.getReturnValue();
+                if(ttp != null){
+                  var nn = ttp.split(":");
+                 }
+                 
+              var  tts = nn[0]+":"+nn[1]+":"+nn[2];
+                
+                component.set("v.ltngTimmer",tts);
+              if(nn[4]=='minus'){
+                        component.set("v.plus","false"); 
+                  console.log(nn[4]);
+                     }
+                     if(nn[4]=='plus'){
+                         component.set("v.plus","true"); 
+                         console.log(nn[4]);
+                     }
+               
+                    var ss = tts.split(":");
+                    var days = ((ss[0]-(ss[0]%24))/24);
+                    component.set("v.days",days);  
+                  
+               
+                 if( nn[3] == 'false'){
+                     
+                      helper.setStartTimeOnUI(component); 
+                 }else{
+                   helper.setStopTimeOnUI(component);
+                 }
+                
+                
+                
+                
+                
+            }});
+        $A.enqueueAction(action);
+        
+        
+    }
+})
---
+++ SLATimer/SLATimerHelper.js
@@ -0,0 +1,66 @@
+({
+	  waitingTimeId: null,
+    setStartTimeOnUI : function(component) {
+         window.clearTimeout(this.waitingTimeId);
+        component.set("v.ltngIsDisplayed",true);
+        var isPositive = component.get("v.plus");
+        var currTime =component.get("v.ltngTimmer");
+         var days;
+    if(typeof currTime !== 'undefined') {
+             var ss = currTime.split(":");
+        
+        var dt = new Date();
+       
+       
+         if(ss[0]>23){
+            days = ((ss[0]-(ss[0]%24))/24);
+          component.set("v.days",days);  
+            ss[0] = (ss[0]%24);
+           
+        }
+        dt.setHours(ss[0]);
+        dt.setMinutes(ss[1]);
+        dt.setSeconds(ss[2]);
+      
+        if(isPositive == "false"){
+           
+            var dt2 = new Date(dt.valueOf() + 1000); 
+        }
+        else{
+            var dt2 = new Date(dt.valueOf() - 1000); 
+            console.log(isPositive +'true')
+        }
+       
+        var temp = dt2.toTimeString().split(" ");
+        var ts = temp[0].split(":");
+      
+        component.set("v.ltngTimmer",ts[0] + ":" + ts[1] + ":" + ts[2]);
+        
+           
+                
+        
+          
+        component.set("v.ltngHour1",ts[0]);
+         component.set("v.ltngMinute1",ts[1]);
+          component.set("v.ltngSecond1",ts[2]);
+        this.waitingTimeId =setTimeout($A.getCallback(() => this.setStartTimeOnUI(component)), 1000);
+        if(ts[0]==0 && ts[1]==0 && ts[2]==0 ){
+            component.set("v.ltngTimmer","EXPIRED");
+            window.clearTimeout(this.waitingTimeId);
+            component.set("v.ltngIsDisplayed",false);
+        }
+    }
+    },
+    setStopTimeOnUI : function(component) {
+        component.set("v.ltngIsDisplayed",false);
+        window.clearTimeout(this.waitingTimeId);
+    },
+    setResetTimeOnUI : function(component) {
+        component.set("v.ltngIsDisplayed",false);
+        component.set("v.ltngHour","10");
+        component.set("v.ltngMinute","0");
+        component.set("v.ltngSecond","0");
+        component.set("v.ltngTimmer","00:00:00");
+        window.clearTimeout(this.waitingTimeId);
+    }
+})
